d5b82793400383bda84cddeb7755e84d097d966e
jose geraldo fernandes <macaverde604@gmail.com>
2022-12-14 22:51:44 -0300

    add plot pareto

--- exaustivo.py
+++ exaustivo.py
@@ -7,8 +7,15 @@ Created on Fri Dec  9 09:37:37 2022
 """
 
 from pareto import rea
+import matplotlib.pyplot as plt
 
 num = 100
 experimento = rea()
 export, border = experimento.pareto(num)
 # export, border = experimento.pareto_naive(num)
+
+# plot pareto
+plt.figure()
+plt.scatter(border[:, 0], border[:, 1])
+plt.xlabel('M')
+plt.ylabel('F')
