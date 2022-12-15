9086e0b5c918b07f49b235ef35e0183868556474
jose geraldo fernandes <macaverde604@gmail.com>
2022-12-14 22:52:16 -0300

    add plot funcional

--- decisao.py
+++ decisao.py
@@ -19,12 +19,19 @@ tau = 1.25
 penalizado = np.copy(border)
 penalizado[:, 1] *= tau # penalizando custo de falha total
 
-soma = np.sum(penalizado, axis = 1)
-plt.plot(soma)
-decisao = export.iloc[np.argmin(soma)].values
+U = np.sum(penalizado, axis = 1)
+decisao = export.iloc[np.argmin(U)].values
 _, counts = np.unique(decisao, return_counts = True)
 
 # alpha, gama
 alpha = counts[0] / len(decisao)
 if 2 in _:
     gama = counts[1] / len(decisao)
+
+# plot
+plt.figure()
+plt.plot(U)
+plt.scatter(np.argmin(U), U[np.argmin(U)], c = '#ff7f0e')
+# plt.axvline(x = np.argmin(U), c = '#ff7f0e')
+plt.xticks([])
+plt.ylabel('U')
